7a25ea3b79e4613d4dcc3872c0d4c098446b093e
zpectral <dspezzk@gmail.com>
2019-11-23 09:26:23 +0100

    Add tabular

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+//const fs = require('fs');
+//const Tabulator = require('tabulator-tables'); 
+
+
+
+
+//let JSONold = fs.readFileSync("./monitors.json");
+//let monitors = JSON.parse(JSONold);
+//console.log(monitors);
