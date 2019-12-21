23e3375ae40fe18ecf0a583fd9e63ee7e1b35add
zpectral <dspezzk@gmail.com>
2019-12-21 09:31:58 +0100

    change layout

--- scripts/main.js
+++ scripts/main.js
@@ -63,38 +63,38 @@ function openCity(evt, targetid) {
 
 var monitoriTable= new Tabulator("#monitori", {
     ajaxURL: myFiles["monitori"],
-    height:"311px",
+    height:"800px",
     layout:"fitColumns",
     //autoColumns:true,
     columns:[
         {title:"Info", field:"itemInfo", sorter:"string"},
-        {title:"Cijena", field:"itemPrice", align:"right", sorter:"number", formatter:"money", formatterParams:{
+        {title:"Cijena", field:"itemPrice", align:"right", width:200, sorter:"number", formatter:"money", formatterParams:{
             decimal:",",
             thousand:".",
             symbol:" kn",
             symbolAfter:" kn",
             precision:0,
         }},
-        {title:"Popust %", field:"percentChange", align:"right", sorter:"number", formatter:"money", formatterParams:{
+        {title:"Popust %", field:"percentChange", align:"right", width:200, sorter:"number", formatter:"money", formatterParams:{
             decimal:",",
             thousand:".",
             symbol:"%",
             symbolAfter:"%",
             precision:0,
         }},
-        {title:"Ukupna promjena", field:"priceChange", align:"right", sorter:"number", formatter:"money", formatterParams:{
+        {title:"Ukupna promjena", field:"priceChange", align:"right", width:200, sorter:"number", formatter:"money", formatterParams:{
             decimal:",",
             thousand:".",
             symbol:" kn",
             symbolAfter:" kn",
             precision:0,
         }},
-        {title:"Dodano", field:"dateAdded", align:"right", sorter:"date", formatter:"datetime", formatterParams:{
+        {title:"Dodano", field:"dateAdded", align:"right", width:200, sorter:"date", formatter:"datetime", formatterParams:{
             inputFormat:"DD-MM-YYYY",
             outputFormat:"DD-MM-YYYY",
             invalidPlaceholder:"(invalid date)",
         }},
-        {title:"Link", field:"itemLink", formatter:"link", formatterParams:{
+        {title:"Link", field:"itemLink", width:100, formatter:"link", formatterParams:{
             label:"link",
             urlField:"itemLink",
             target:"_blank",
@@ -105,7 +105,7 @@ var monitoriTable= new Tabulator("#monitori", {
 
 var procesoriTable= new Tabulator("#procesori", {
     ajaxURL: myFiles["procesori"],
-    height:"311px",
+    height:"800px",
     layout:"fitColumns",
     //autoColumns:true,
     columns:[
